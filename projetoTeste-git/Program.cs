106124b13d249367dff199cb97f3532bd1dce782
Lucas <lucassilveira7@outlook.com>
2018-10-23 15:44:44 -0300

    primeiro commit

---
+++ projetoTeste-git/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace projetoTeste_git {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
