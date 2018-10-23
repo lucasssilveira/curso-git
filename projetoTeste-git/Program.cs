73b5b282b2acc8b19587ec4042c01d03dbaa4418
Lucas <lucassilveira7@outlook.com>
2018-10-23 15:52:19 -0300

    segundo commit

--- projetoTeste-git/Program.cs
+++ projetoTeste-git/Program.cs
@@ -4,6 +4,7 @@ namespace projetoTeste_git {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Good morning!");
             Console.ReadLine();
         }
     }
