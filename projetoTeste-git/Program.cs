9407aa9545baead2fdfbdad2c60f23c488a418b4
Lucas <lucassilveira7@outlook.com>
2018-10-24 14:18:45 -0300

    how are you today?

--- projetoTeste-git/Program.cs
+++ projetoTeste-git/Program.cs
@@ -4,9 +4,9 @@ namespace projetoTeste_git {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Good morning!");
-            Console.WriteLine("pqp!");
-            Console.WriteLine("Good morning!");
+         
+            Console.WriteLine("How are you , today?");
+          
             Console.ReadLine();
         }
     }
